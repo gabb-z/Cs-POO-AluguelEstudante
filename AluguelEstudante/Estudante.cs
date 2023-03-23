519e485da97effb54470d7ee45a75aafa6fac6c3
gabb-z <gabbz.liima@gmail.com>
2023-03-22 22:01:35 -0300

    v1 - Projeto AluguelEstudante

---
+++ AluguelEstudante/Estudante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AluguelEstudante
+{
+    class Estudante
+    {
+        //Atributos da Classe - Estudante
+        public string NomeEstudante { get; set; }
+        public string EmailEstudante { get; set; }
+
+        //Construtor - Estudante
+        public Estudante(string nomeEstudante, string emailEstudante)
+        {
+            NomeEstudante = nomeEstudante;
+            EmailEstudante = emailEstudante;
+        }
+
+        //ToString - Infos Estudante
+        public override string ToString()
+        {
+            return NomeEstudante + ", " + EmailEstudante;
+        }
+    }
+}
---
+++ AluguelEstudante/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AluguelEstudante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de Obejto - Vetor de Quartos
+            Estudante[] estudante = new Estudante[10];
+
+            //Entrada de Dados - Quartos à Alugar
+            Console.Write("Quantos quartos deseja alugar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Condição - Infos do Estudante
+            for (int i = 1; i <= n; i++)
+            {
+                //Entrada de Dados - Infos do Estudante
+                Console.WriteLine($"\nAluguel #{i}:");
+                Console.Write("Nome: ");
+                string nomeEstudante = Console.ReadLine();
+                Console.Write("Email: ");
+                string emailEstudante = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quartoEstudante = int.Parse(Console.ReadLine());
+                estudante[quartoEstudante] = new Estudante(nomeEstudante, emailEstudante);
+            }
+
+            //Saída de Dados - Quarto(s) Ocupado(s)
+            Console.WriteLine("\nQuarto(s) ocupado(s): ");
+
+            //Condição - Quarto(s) Ocupado(s)
+            for (int i = 0; i < 10; i++)
+            {
+                if (estudante[i] != null)
+                {
+                    //Saída de Dados - Quarto(s) Estudante(s)
+                    Console.WriteLine(i + ": " + estudante[i]);
+                }
+            }
+        }
+    }
+}
